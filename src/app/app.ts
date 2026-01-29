a2d2f2ffba75d9055468713952d867aaccee1686
David <davidevis1266@gmail.com>
2026-01-29 18:53:36 +0100

    Schedule Terminado

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,12 +8,28 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  showform: boolean = false;
+  showform: boolean = true;
+  showScheduleScissors: boolean= false;
+  showScheduleBeard: boolean = false;
   
   showForm(){
     this.showform=false;
+    this.showScheduleScissors=false
+    this.showScheduleBeard=false;
   }
   showMain(){
-    return this.showform= true;
+    this.showform= true;
+    this.showScheduleScissors=false;
+    this.showScheduleBeard=false;
+
   }
+  showSchedule1(){
+    this.showScheduleScissors=true;
+    this.showScheduleBeard=false;
+  }
+  showSchedule2(){
+    this.showScheduleBeard=true;
+    this.showScheduleScissors=false;
+  }
+
 }
